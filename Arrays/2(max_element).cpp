5b8e1536fbda68169b9ab7ff40b9f3d2b667a24f
Akash Kumar Singh <83223139+akashkumarSingh34@users.noreply.github.com>
2022-08-05 10:37:54 +0530

    Create 2(max_element).cpp

---
+++ Arrays/2(max_element).cpp
@@ -0,0 +1,10 @@
+//assuming that the array contains atleast one element
+
+
+int maax_element(int arr[],int n){
+  int max=arr[0];
+  for(int i=1; i<n; i++)
+    if(max<arr[i])
+      max=arr[i];
+  return max;
+}
